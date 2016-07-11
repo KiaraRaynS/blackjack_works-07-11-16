cd70a63e0a441bc1c3bda7e4be19b473b52d5fe1
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-11 18:11:13 -0400

    Fixed bug

--- create_player.py
+++ create_player.py
@@ -16,20 +16,27 @@ class Person():
         hand_int_values = []
         integers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         ace_in_hand = False
+        game_won = False
         for value in self.card_values:
             if value in integers:
                 hand_int_values.append(value)
             if value in jqk:
                 hand_int_values.append(10)
             if value == 'A':
-                ace_in_hand = True
+                if not ace_in_hand:
+                    ace_in_hand = True
+                else:
+                    game_won = True
         current_value = sum(hand_int_values)
         if ace_in_hand:
             if current_value + 11 <= 21:
                 current_value + 11
             else:
                 current_value + 1
-        self.hand_value = current_value
+        if not game_won:
+            self.hand_value = current_value
+        else:
+            self.hand_value = 21
         return self.hand_value
 
 
